a7fe397ef99c3607484ac6cc25a7d3a0b3352ded
samun4u <samunmollik86@gmail.com>
2023-12-01 11:16:24 +0600

    create OAuth  component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -46,4 +46,36 @@ export const signin = async(req,res,next) =>{
 
 }
 
+//google
+export const google = async(req,res,next) => {
+    try{
+        const { name, email, photo } = req.body;
+        const user = await User.findOne({ email });
+        if(user){
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const {password: pass, ..._user} = user._doc;
+            res.cookie('access_token', token,{ httpOnly: true}).status(200).json(_user);
+        }else{
+            username = name.split(" ").join("").toLowerCase() + Math().random().toSting(36).slice(-4);
+            const generatedPassword = Math().random().toSting(36).slice(-8) + Math().random().toSting(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+
+            const newUser = new User({
+                username,
+                email,
+                password:  hashedPassword,
+                avatar: photo, 
+            });
+            await newUser.save();
+
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const {password: pass, ..._user} = newUser._doc;
+            res.cookie('access_token', token, {httpOnly: true}).status(200).json(_user);
+
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
 
---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-real-estate-36be1.firebaseapp.com",
+  projectId: "mern-real-estate-36be1",
+  storageBucket: "mern-real-estate-36be1.appspot.com",
+  messagingSenderId: "721713538101",
+  appId: "1:721713538101:web:8222cb1fe9fab3cabd3d7c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
