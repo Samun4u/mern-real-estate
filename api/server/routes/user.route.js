e206c03b7af8244bb4822e77bf10841bde2e2e2b
Samun Mollik <samunmollik86@gmail.com>
2025-01-31 15:08:41 +0600

    Add contact landload functionality to listing page

--- api/server/routes/user.route.js
+++ api/server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { deleteUser, updateUser, getUserListings } from '../controllers/user.controller.js';
+import {getUser, deleteUser, updateUser, getUserListings } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/update/:id', verifyUser, updateUser);
 router.delete('/delete/:id', verifyUser, deleteUser);
 router.get('/listings/:id', verifyUser, getUserListings);
+router.get('/:id', verifyUser, getUser);
 
 export default router;
